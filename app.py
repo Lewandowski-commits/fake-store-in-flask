08de97a5e55b67083eac0b10c8f6b9e008ade629
lewandowskicommits <vesilth@gmail.com>
2021-08-25 23:54:52 +0200

    add random sample of products on home

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from random import sample
 import requests
 
 app = Flask(__name__)
@@ -6,27 +7,31 @@ app = Flask(__name__)
 productsUrl = 'https://fakestoreapi.com/products'
 categoriesUrl = 'https://fakestoreapi.com/products/categories'
 
+
 @app.route('/')
 def home():
-    resp = requests.get(url=productsUrl+'?limit=5')
-    productsList = resp.json()
+    resp = requests.get(url=productsUrl)
+    productsListPreSel = resp.json()
+    productsList = sample(productsListPreSel, 3)
 
     resp = requests.get(url=categoriesUrl)
     categoriesList = resp.json()
 
-
     return render_template('homepage.html',
                            categoriesList=categoriesList,
                            productsList=productsList)
+
+
 @app.route('/category/<selected_cat>')
 def category(selected_cat):
-    resp = requests.get(url=productsUrl + '/category/'+ selected_cat)
+    resp = requests.get(url=productsUrl + '/category/' + selected_cat)
     productsList = resp.json()
     return render_template('category.html',
                            productsList=productsList,
                            selected_cat=selected_cat)
 
+
 if __name__ == '__main__':
     app.run(debug=True,
             host='0.0.0.0',
-            port= 8090)
+            port=8090)
