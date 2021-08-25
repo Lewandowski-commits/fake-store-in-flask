afb78952709f979928f715cd7982288b52301b3e
lewandowskicommits <vesilth@gmail.com>
2021-08-25 23:03:39 +0200

    add home template and top products card

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ productsUrl = 'https://fakestoreapi.com/products'
 categoriesUrl = 'https://fakestoreapi.com/products/categories'
 
 @app.route('/')
-def base():
-    resp = requests.get(url=productsUrl)
+def home():
+    resp = requests.get(url=productsUrl+'?limit=5')
     productsList = resp.json()
 
     resp = requests.get(url=categoriesUrl)
     categoriesList = resp.json()
 
 
-    return render_template('base.html', categoriesList=categoriesList)
+    return render_template('homepage.html',
+                           categoriesList=categoriesList,
+                           productsList=productsList)
 
 if __name__ == '__main__':
     app.run(debug=True,
